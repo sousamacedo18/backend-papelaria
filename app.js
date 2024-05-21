2cccc5965049e26655408a38359006d1b9cfa004
Carlos Henrique Sousa de Macedo <carlossousa@sistemafieto.rede>
2024-05-21 08:20:21 -0300

    Execicios de array

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
-const cep ={
+const cep =[
+    {
     "cep": "01001-000",
     "logradouro": "Praça da Sé",
     "complemento": "lado ímpar",
@@ -11,7 +12,32 @@ const cep ={
     "gia": "1004",
     "ddd": "11",
     "siafi": "7107"
+  },
+  {
+    "cep": "77814-790",
+    "logradouro": "Rua 20",
+    "complemento": "",
+    "bairro": "Loteamento Monte Sinai",
+    "localidade": "Araguaína",
+    "uf": "TO",
+    "ibge": "1702109",
+    "gia": "",
+    "ddd": "63",
+    "siafi": "9241"
+  },
+  {
+    "cep": "77825-769",
+    "logradouro": "Rua dos Avelos",
+    "complemento": "",
+    "bairro": "Residencial Topázio",
+    "localidade": "Araguaína",
+    "uf": "TO",
+    "ibge": "1702109",
+    "gia": "",
+    "ddd": "63",
+    "siafi": "9241"
   }
+]
 const usuario=[
     {nome:'carlos', idade:'20',peso:'100'},
     {nome:'pedro', idade:'30'},
@@ -23,9 +49,44 @@ app.use("/todos",(req, res, next)=>{
 app.use("/nome",(req, res, next)=>{
     res.send(usuario[1].nome)
 })
-app.use("/cep",(req, res, next)=>{
-    res.send(cep)
+app.use("/cep/:valor",(req, res, next)=>{
+    const valor=req.params.valor
+    const cepfiltrado = cep.filter(linha=>{
+       if(linha.cep===valor)
+       { return linha}
+    })
+    res.send(cepfiltrado)
 })
+// https://viacep.com.br/ws/01001000/json/
+    app.use("/viacep/:valor",(req, res, next)=>{
+        const valor=req.params.valor;
+    fetch("https://viacep.com.br/ws/"
+                    +valor+"/json")
+                    .then(resposta=>
+                        
+                            resposta.json()
+                            //"cep,logradouro, localidade"                                
+                    ).then(data=>{
+                        const dados={
+                            cep:data.cep,
+                            logradouro:data.logradouro,
+                            localidade:data.localidade
+                        }
+                        res.send(dados)
+                    })
+    })
+
+const alunos=[
+    "ana silva",
+    "carlos silva"
+]
+alunos[1]
+const mat={'idaluno':'1','idcurso':'3'}
+const matricula=[
+    {'idaluno':'1','idcurso':'3'},
+    {"idaluno":'3', "idcurso:":"5"}
+]
+
 app.use("/soma",(req, res, next)=>{
     let total=0;
     
